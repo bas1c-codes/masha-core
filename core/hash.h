ec9ebb01185ec65efe4afa92a5314f33bdea0436
Adhul <adhul410@gmail.com>
2025-06-02 13:14:26 +0530

    Initial commit: added core hashing and CLI

--- core/hash.h
+++ core/hash.h
@@ -1,8 +1,11 @@
 #ifndef HASH_H
 #define HASH_H
+
 #include <string>
-class Hash{
-    public:
-        std::string hash(const std::string& name);
+
+class Hash {
+public:
+    std::string hash(const std::string& name);
 };
-#endif 
+
+#endif // HASH_H
