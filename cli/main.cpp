0ec227546c23ec6947462f364db1c627df4e36e6
Adhul <adhul410@gmail.com>
2025-06-04 14:14:21 +0530

    Added db (Might remove it later)

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "../core/hash.h"
+#include "../core/scan.h"
 #include "CLI/CLI.hpp"
 int main(int argc, char **argv){
-    Hash hash;
+    Scan scan;
     CLI::App app;
     std::string path;
     app.add_option("-s",path,"Scan the file");
     CLI11_PARSE(app, argc, argv);
-    std::string res=hash.hash(path);
+    std::string res=scan.scan(path);
     std::cout << res;
     
     return 0;     
--- core/scan.cpp
+++ core/scan.cpp
@@ -1,7 +1,38 @@
 #include "scan.h"
 #include <iostream>
-
-
+#include <sqlite3.h>
+#include "hash.h"
 std::string Scan::scan(const std::string& path) {
-    return "";
+    sqlite3* db;
+    Hash hash;
+    if (sqlite3_open("D:/Softwares/Proj/masha/core/malware_hashes.db", &db) != SQLITE_OK) { /*db open error*/
+    return "Failed to open database";
+    }
+    sqlite3_stmt* stmt;
+    std::string sha256  = hash.hash(path);
+    const char* query = "SELECT 1 FROM malware_hashes WHERE sha256 = ""?"" LIMIT 1;";
+
+    if(sqlite3_prepare(db,query,-1,&stmt,nullptr) !=SQLITE_OK){ /*statement preparation error*/
+        std::cerr << "Failed to prepare statement" << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return "DB closed";
+    }
+    
+    if (sqlite3_bind_text(stmt, 1, sha256.c_str(), -1, SQLITE_STATIC) != SQLITE_OK) { /*bind error*/
+        std::cerr << "Failed to bind hash\n";
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        return "Binding error";
+    }
+    
+    int rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW) {  /*checking for hash if SQLITE_ROW exists it means theres some value*/
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        return "Not found";
+    }
+    sqlite3_finalize(stmt);
+    return "Malware found";
+
+    
 }
